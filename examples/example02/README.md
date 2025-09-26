603c4d380a256e03cfb5e5f8ee556573d6242b8c
Miguel Angel Timana Paz <migueltimanapaz@gmail.com>
2025-09-26 17:48:10 +0000

    adding example02

---
+++ examples/example02/README.md
@@ -0,0 +1,25 @@
+## Example using Gemfile:
+# 1. Install Bundler (if you don't have it)
+```
+gem install bundler
+```
+
+# 2. Install dependencies
+```
+bundle install
+```
+
+# 3. Check installed dependencies
+```
+bundle list
+```
+
+# 4. Check that everything is correct
+```
+bundle check
+```
+
+# 5. Execute commands in the context of the bundle
+```
+bundle exec ruby main.rb
+```
---
+++ examples/example02/main.rb
@@ -0,0 +1,12 @@
+require 'huaweicloud-obs'  
+require 'dotenv/load'
+
+access_key = ENV['HUAWEICLOUD_ACCESS_KEY']
+secret_key = ENV['HUAWEICLOUD_SECRET_KEY']
+region     = ENV['HUAWEICLOUD_REGION']
+
+obs = OBS::Client.new(access_key, secret_key, region)
+response = obs.putObject('file01.txt', 'bucket-huawei-demo', './file.txt')
+#response = obs.putObject('archivos/file02.txt', 'bucket-huawei-demo', './file.txt')
+#response = obs.deleteObject('archivos/file02.txt', 'bucket-huawei-demo')
+#response = obs.getObject('file01.txt', 'bucket-huawei-demo','./file01.txt')
