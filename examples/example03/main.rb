01cc8f26864a2dafc5276f0fcfea65ed4a8582c2
Miguel Angel Timana Paz <miguel.timana.outs@h-partners.com>
2025-10-20 10:32:19 -0500

    adding cdn

--- examples/example03/main.rb
+++ examples/example03/main.rb
@@ -6,4 +6,4 @@ secret_key = ENV['HUAWEICLOUD_SECRET_KEY']
 region     = ENV['HUAWEICLOUD_REGION']
 
 client = CDN::Client.new(region: region,access_key: access_key, secret_key: secret_key)
-client.create_invalidation('https://qas.plan4us.pe/','https://qas.plan4us.pe/FILES/')#,'https://qas.plan4us.pe/FILES/'
+client.create_invalidation('https://miempresaabc.link/','https://miempresaabc.link/FILES/')
